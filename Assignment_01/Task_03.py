2d2f96dbe6e9cd24c4f28abf560952d3b4b5a09f
Ansari-Codes <ansaricodes@gmail.com>
2026-03-23 12:28:56 +0500

    Task_03 completed

--- Assignment_01/Task_03.py
+++ Assignment_01/Task_03.py
@@ -1,12 +1,50 @@
+'''
+This Python file is for Task 03, stated as:
+
+- Build one small AI tool using Python + LangChain.
+- Examples: AI Study Assistant, AI Email Generator, or AI Idea Generator.
+- The tool should take user input and generate helpful output.
+
+What m gonna do? I will build a "Wikipedia Info Retriever"
+- User will enter the topic
+- User will choose the language
+- User will get the information on that topic retrieved from wikipedia
+Feature: Interactive response with bullet points and explainations, easy to understand, tri-lingual
+Note: Only chinese, english and urdu are supported yet
+'''
+
+'''
+Required Tools:
+
+- langchain and related libraries
+- wikipedia tools and utilities
+- progressive_py library: A library to create interactive cli visuals for loading (created by me 😏, you can get it on github: https://github.com/Ansari-Codes/progressive_py)
+'''
+
 #########################################
 ## STEP 1: Import required libraries   ##
 #########################################
 
 from progressive_py.spinner import Spinner, hide_cursor, show_cursor
-spinner = Spinner()
+from progressive_py.manager import AssetsManager
+
+try:
+    manager = AssetsManager()
+    loading_spinner_style = manager.load("spinner", "style", "dotsCircle")
+    loading_spinner_theme = manager.load("spinner", "theme", "matrix")
+    llm_spinner_style = manager.load("spinner", "style", "bouncingBall")
+    llm_spinner_theme = manager.load("spinner", "theme", "cyberpunk")
+except Exception as e:
+    print(e)
+    loading_spinner_style =  ['|', '/', '-', '\\']
+    loading_spinner_theme = {}
+    llm_spinner_style =  ['|', '/', '-', '\\']
+    llm_spinner_theme = {}
+
+spinner = Spinner(seq=loading_spinner_style, spn_side="left", interval=0.08, **loading_spinner_theme)
 spinner.start()
 spinner.final_text = '<=== Wikipedia Info Retriever ===>'
-spinner.text = "Initializing: Loading Libraries..."
+spinner.text = "Loading Libraries..."
 
 from langchain_openai import ChatOpenAI
 from langchain_community.tools import WikipediaQueryRun
@@ -18,14 +56,14 @@ from dotenv import load_dotenv
 ## STEP 2: Load environment  ##
 ###############################
 
-spinner.text = "Initializing: Loading env Variables..."
+spinner.text = "Loading env Variables..."
 load_dotenv()
 
 ################################
 ## STEP 3: Initialize the LLM ##
 ################################
 
-spinner.text = "Initializing: Initializing llm..."
+spinner.text = "Initializing llm object..."
 llm = ChatOpenAI(
     temperature=0.3,  # lower = more factual
     model="glm-4.7-flash",
@@ -36,7 +74,7 @@ llm = ChatOpenAI(
 ## STEP 4: Setup Wikipedia Retriever    ##
 ##########################################
 
-spinner.text = "Initializing: Creating wiki tool instances..."
+spinner.text = "Initializing wiki tool instances..."
 wiki = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=5000)
 wiki_tool = WikipediaQueryRun(api_wrapper=wiki)
 
@@ -44,7 +82,7 @@ wiki_tool = WikipediaQueryRun(api_wrapper=wiki)
 ## STEP 5: Create Prompt Template ##
 ####################################
 
-spinner.text = "Initializing: Crafting prompt template..."
+spinner.text = "Crafting prompt template..."
 prompt = PromptTemplate(
     input_variables=["topic", "wiki_data"],
     template="""
@@ -62,7 +100,10 @@ Your task:
 - Keep it informative and structured
 - Language of response should be {language}
 
-Format:
+Follow this Format:
+
+Overview:
+...
 
 Explanation:
 ...
@@ -77,7 +118,7 @@ Key Points:
 ## STEP 6: Create Chain ##
 ##########################
 
-spinner.text = "Initializing: Defining chain..."
+spinner.text = "Defining chain..."
 chain = prompt | llm
 
 spinner.stop()
@@ -88,8 +129,8 @@ spinner.stop()
 
 if __name__ == "__main__":
     while True:
-        topic = input("Enter topic (or type 'exit'): ")
-        language = input("Language (ur/en/zh): ")
+        topic = input(">>> Enter topic (or type 'exit'): ")
+        language = input(">>> Language (ur/en/zh): ")
         language = language.lower().strip() or 'en'
         if language not in ['ur', 'en', 'zh']:
             print("Language can only be en (english), urdu (ur) or chinese (zh).")
@@ -98,7 +139,7 @@ if __name__ == "__main__":
             print("Exiting...")
             break
         hide_cursor()
-        spn = Spinner()
+        spn = Spinner(seq=llm_spinner_style, spn_side="left", interval=0.08, **llm_spinner_theme)
         spn.show(); spn.start()
         spn.text = "Retrieving data..."
         wiki_data = wiki_tool.run(topic)
@@ -106,9 +147,10 @@ if __name__ == "__main__":
         response = chain.invoke({
                 "topic": topic,
                 "wiki_data": wiki_data,
-                "language": {"ur": "Urdu", "en": "English", "zh": "Chinese Simplified"}[language]
+                "language": {"ur": "Urdu", "en": "English", "zh": "Chinese (Simplified)"}[language]
             })
         show_cursor()
-        spn.final_text = response.content
+        spn.final_text = ">>> Response"
         spn.stop(); spn.hide()
+        print(response.content)
         print('-'*60)
